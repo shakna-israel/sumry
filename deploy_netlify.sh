e5efbaa3c98906e960592798e026e78ef4451c7a
James Milne <s4b3r6@gmail.com>
2021-01-07 04:49:21 +1100

    Fix pipeline

--- deploy_netlify.sh
+++ deploy_netlify.sh
@@ -9,5 +9,4 @@ zip -q -r ../website.zip .
 curl -s -H "Content-Type: application/zip" \
      -H "Authorization: Bearer ${auth}" \
      --data-binary "@website.zip" \
-     https://api.netlify.com/api/v1/sites/sumry.netlify.app/deploys \
-> /dev/null
+     https://api.netlify.com/api/v1/sites/sumry.netlify.app/deploys
