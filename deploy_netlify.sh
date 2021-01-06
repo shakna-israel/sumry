3283e33691f5813befe828e8c29c171f9b323772
James Milne <s4b3r6@gmail.com>
2021-01-07 04:48:04 +1100

    Build pipeline

---
+++ deploy_netlify.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+zip -q -r ../website.zip .
+
+. ~/auth_file
+
+curl -s -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer ${auth}" \
+     --data-binary "@website.zip" \
+     https://api.netlify.com/api/v1/sites/sumry.netlify.app/deploys \
+> /dev/null
